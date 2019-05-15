35a52dc78ab167e6387df9d5b6f1bde1fa749e9a
JimmyChiu702 <jimmychiu702@gmail.com>
2019-05-15 23:20:35 +0800

    2019-05-15 aws

--- client/src/api/light.js
+++ client/src/api/light.js
@@ -1,21 +1,72 @@
-import axios from 'axios';
+var AWS = require('aws-sdk');
+AWS.config.update({region: 'us-east-1'});
+
+var sqs = new AWS.SQS({apiVersion: '2012-11-05'});
 
 export function getLightState() {
-    let url = 'getLightState';
-    return axios.get(url).then(res => {
-        if (res.status !== 200)
-            throw new Error(`Unexpected response code: ${res.status}`);
-        return res.data;
+    let context = {
+        newLightState: null,
+        getLightState: true
+    };
+    let sendParams = {
+        DelaySeconds: 10,
+        MessageAttributes: {},
+        MessageBody: JSON.stringify(context),
+        QueueUrl: "https://sqs.us-east-1.amazonaws.com/915824420250/trytrylook"
+    };
+    sqs.sendMessage(sendParams, (err, data) => {
+        if (err)
+            throw new Error(err.message);
+        let receiveParams = {
+            AttributeNames: [
+                "SentTimestamp"
+            ],
+            MaxNumberOfMessages: 10,
+            MessageAttributeNames: [
+                "All"
+            ],
+            QueueUrl: "https://sqs.us-east-1.amazonaws.com/915824420250/trytrylook",
+            VisibilityTimeout: 20,
+            WaitTimeSeconds: 10
+        };
+        sqs.receiveMessage(receiveParams, (err, data) => {
+            if (err)
+                throw new Error(err.message);
+            return data.Message[0]['Body'];
+        });
     });
 }
 
 export function switchLight(newLightState) {
-    let url = 'switchLight';
-    return axios.post(url, {
-        newLightState: newLightState
-    }).then(res => {
-        if (res.status !== 200)
-            throw new Error(`Unexpected response code: ${res.status}`);
-        return res.data;
+    let context = {
+        newLightState: newLightState,
+        getLightState: false
+    };
+    let sendParams = {
+        DelaySeconds: 10,
+        MessageAttributes: {},
+        MessageBody: JSON.stringify(context),
+        QueueUrl: "https://sqs.us-east-1.amazonaws.com/915824420250/trytrylook"
+    };
+    sqs.sendMessage(sendParams, (err, data) => {
+        if (err)
+            throw new Error(err.message);
+        let receiveParams = {
+            AttributeNames: [
+                "SentTimestamp"
+            ],
+            MaxNumberOfMessages: 10,
+            MessageAttributeNames: [
+                "All"
+            ],
+            QueueUrl: "https://sqs.us-east-1.amazonaws.com/915824420250/trytrylook",
+            VisibilityTimeout: 20,
+            WaitTimeSeconds: 10
+        };
+        sqs.receiveMessage(receiveParams, (err, data) => {
+            if (err)
+                throw new Error(err.message);
+            return data.Message[0]['Body'];
+        });
     });
 }
